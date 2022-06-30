bd83fd597fb996b9436f5cfb1f781d72c7034579
Brian <9bbrian@gmail.com>
2022-06-30 16:09:13 -0700

    add navigation pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import Results from './components/Results';
+import NotFound from './components/NotFound';
+
+class App extends Component {
+  render() {
+    return (
+       <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="results" element={<Results />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Results.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Results = () => {
+    return (
+       <div>
+          <h1>Results</h1>
+          <p>Should navigate away if no results</p>
+       </div>
+    );
+}
+
+export default Results;
